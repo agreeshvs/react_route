0e7bee61b7e93627cb2f0a81ef0e32166d546fc7
agreeshvs <agreesh777@gmail.com>
2023-11-21 14:41:56 +0530

    Nested Routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { Route,Routes } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
 import { About } from './components/About';
 import { Navbar } from './components/Navbar';
+import { OrderSummary } from './components/OrderSummary';
+import { NoMatch } from './components/NoMatch';
+import { Products } from './components/Products';
+import { FeaturedProducts } from './components/FeaturedProducts';
+import { NewProducts } from './components/NewProducts';
 
 function App() {
   return (
@@ -11,6 +16,12 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='about' element={<About />} />
+        <Route path='order-summary' element={<OrderSummary />} />
+        <Route path='*' element={<NoMatch />} />
+        <Route path='products' element={<Products />} >
+          <Route path='featured' element={<FeaturedProducts />}/>
+          <Route path='new' element={<NewProducts />} />
+        </Route>
       </Routes>
     </>
     
