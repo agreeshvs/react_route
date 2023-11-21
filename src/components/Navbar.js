3dc3005d38c774d23596dc26f48984c0841beadd
agreeshvs <agreesh777@gmail.com>
2023-11-21 11:09:32 +0530

    Active link for navigation.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 export const Navbar = () => {
+    const navLinkStyle = ( {isActive}) => {
+        return {
+            fontWeight: isActive ? 'bold' : 'normal',
+            textDecoration: isActive ? 'none' : 'underline'
+        }
+    }
   return (
     <nav>
-        <Link to='/' >Home</Link>
-        <Link to='/about'>About</Link>
+        <NavLink 
+            style={navLinkStyle} to='/' >Home</NavLink>
+        <NavLink 
+            style={navLinkStyle} to='/about'>About</NavLink>
     </nav>
   )
 }
