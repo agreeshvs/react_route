7f21187667c3645d3edfe595dbe17b8306f9f956
agreeshvs <agreesh777@gmail.com>
2023-11-21 16:58:58 +0530

    Dynamic Routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import { Route,Routes } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
 import { About } from './components/About';
 import { Navbar } from './components/Navbar';
+import { OrderSummary } from './components/OrderSummary';
+import { NoMatch } from './components/NoMatch';
+import { Products } from './components/Products';
+import { FeaturedProducts } from './components/FeaturedProducts';
+import { NewProducts } from './components/NewProducts';
+import { Users } from './components/Users';
+import { UserDetails } from './components/UserDetails';
+import { Admin } from './components/Admin';
 
 function App() {
   return (
@@ -11,6 +19,17 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='about' element={<About />} />
+        <Route path='order-summary' element={<OrderSummary />} />
+        <Route path='products' element={<Products />} >
+          <Route index element={<FeaturedProducts />}/>
+          <Route path='featured' element={<FeaturedProducts />}/>
+          <Route path='new' element={<NewProducts />} />
+        </Route>
+        <Route path='users' element={<Users />} >
+          <Route path=':userId' element={<UserDetails />} />
+          <Route path='admin' element={<Admin />} />
+        </Route>
+        <Route path='*' element={<NoMatch />} />
       </Routes>
     </>
     
