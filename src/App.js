62a68f330ec6cc042aa1c143497e8d7f27c82549
agreeshvs <agreesh777@gmail.com>
2023-11-21 11:29:13 +0530

    Added rout for order-summary

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route,Routes } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
 import { About } from './components/About';
 import { Navbar } from './components/Navbar';
+import { OrderSummary } from './components/OrderSummary';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='about' element={<About />} />
+        <Route path='order-summary' element={<OrderSummary />} />
       </Routes>
     </>
     
