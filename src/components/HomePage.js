c9020a0218aebac47223323ee4af2d6f7cbf6967
agreeshvs <agreesh777@gmail.com>
2023-11-21 11:28:25 +0530

    Navigating Programmatically

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 export const HomePage = () => {
+  const navigate = useNavigate()
+  // {replace: true} -> Replace the history
   return (
-    <div>HomePage</div>
+    <>
+      <div>HomePage</div>
+      <button onClick={() => navigate('order-summary',{replace: true})}>Place Order</button>
+    </>
   )
 }
